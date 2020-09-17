4d8c7c99859b0bde9eae4a6cce08432f16b3f9e7
Martin Cervantes <cervantes.martine@gmail.com>
2020-09-17 10:44:59 -0600

    Add phaser initial configuration

---
+++ src/config/config.js
@@ -0,0 +1,8 @@
+import 'phaser';
+
+export default {
+  type: Phaser.AUTO,
+  parent: "VideoGame_JS",
+  width: 800,
+  height: 600
+};
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import 'phaser';
+import config from './config/config';
+
+import BootScene from './scenes/BootScene';
+
+class Game extends Phaser.Game {
+  constructor () {
+    super(config);
+    this.scene.add('Boot', BootScene);
+
+    this.scene.start('Boot');
+  }
+}
+
+window.game = new Game();
