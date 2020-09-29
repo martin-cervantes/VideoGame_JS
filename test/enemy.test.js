f40a42e35bc2e70e261c692c8a65327617b09778
Martin Cervantes <cervantes.martine@gmail.com>
2020-09-29 15:49:31 -0600

    Add enemy class testing cases

---
+++ test/enemy.test.js
@@ -0,0 +1,15 @@
+import Enemy from '../src/game/enemy';
+
+const enemy = new Enemy(scene, 800, 200, 'mineAnimation');
+
+test('Check enemy danger', () => {
+  expect(enemy.danger).toBe(50);
+});
+
+test('Check enemy value of points', () => {
+  expect(enemy.value).toBe(100);
+});
+
+test('Check enemy speed to move foward', () => {
+  expect(enemy.speed).toBe(1.5);
+});
