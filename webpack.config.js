28b405db3bbdb52271a52cc7ee6567acb93c3a82
Martin Cervantes <cervantes.martine@gmail.com>
2020-09-29 15:48:19 -0600

    Add testing configuration files

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: "development",
-  entry: './src/index.js',
+  entry: [
+    'regenerator-runtime/runtime',
+    './src/index.js'
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
